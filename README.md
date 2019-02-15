2e9e101c202f0fbdc1faead8f4ac20101cf43dad
Miecrosoft <herberts007@gmail.com>
2019-02-15 18:49:05 +0700

    Initiated the laziness

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Elasticsearch Lazy
+
+```sh
+python3 elasticsearch-lazy -H [IP_ADDRESS] -P [PORT] -a API -e [Execute] -n [Name]
+```
--- elasticsearch-lazy.py
+++ elasticsearch-lazy.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-import sys
 import argparse
-
-# from elasticsearch import Elasticsearch
-import setmap
+import json
+from elasticsearch import Elasticsearch
+obj = object
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-H", help="HOST")
-parser.add_argument("-P", help="PORT")
+parser.add_argument("-H", help = "HOST localhost as default", default="localhost")
+parser.add_argument("-P", help = "PORT 9200 as default", default="9200")
+parser.add_argument("-a", help = "Action executed such as status, create, delete, etc")
+parser.add_argument("-e", help = "Action executed such as status, create, delete, etc")
+parser.add_argument("-n", help = "Action executed such as status, create, delete, etc")
 args = parser.parse_args()
 
-if args.H is None:
-  print ("localhost")
-else:
-  print (args.H)
+es = Elasticsearch(host = "10.164.5.117", port = "9200")
 
-if args.P is None:
-  print ("9200")
-else:
-  print (args.P)
+def create(api, param):
+  res = api.create(index = param)
+  print(json.dumps(res, indent = 2, sort_keys = False))
+
+def delete(api, param):
+  res = api.delete(index=param)
+  print(json.dumps(res, indent = 2, sort_keys = False))
+
+if args.a == "indices":
+  obj = es.indices
+
+if args.e == "create":
+  create(obj, args.n)
+elif args.e == "delete":
+  delete(obj, args.n)
